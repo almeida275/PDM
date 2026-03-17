cd4a6b6d88a257f0ed99390ade31cf0be1bfe365
josenildo almeida <andre.almeida@ifpb.edu.br>
2026-03-17 11:30:42 -0300

    projeto inicial

---
+++ Documents/pdm2026/app/index.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { TextInput, View, Text } from "react-native"
+
+export default function Index() {
+  const [nome, setNome]= useState('');
+  return (
+    <View
+      style={{
+        flex: 1,
+        padding: 16,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+ <TextInput
+ placeholder="Digite Algo..."
+ onChangeText={ novoTexto => setNome(novoTexto) }
+ value={nome}
+ style={{
+ width: "100%",
+ padding: 5,
+ borderWidth: 1
+}}
+ />
+ <Text>{nome}</Text>
+    </View>
+  );
+}
